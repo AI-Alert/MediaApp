f221279457c9baca5ef7618f4ba2a54e90a66c96
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2022-04-26 20:07:26 +0200

    Fourth Commit

--- app/src/main/java/com/example/mediaapp/CameraActivity.java
+++ app/src/main/java/com/example/mediaapp/CameraActivity.java
@@ -1,14 +1,47 @@
 package com.example.mediaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    // --Commented out by Inspection (16.05.2021 9:34):Button button;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+
     }
 }
